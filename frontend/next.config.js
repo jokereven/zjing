d865765c3c8b5a07188afaae3698e1792c658fbb
jokereven <zhou125disorder@163.com>
2022-03-05 22:02:57 +0800

    feat(blog): update the blog :tada:
    now have one bug there have something is not enable :bug:

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	devIndicators: {
+		autoPrerender: false,
+	},
+};
--- frontend/pages/blog/index.js
+++ frontend/pages/blog/index.js
@@ -1,3 +1,35 @@
-export default function Blog() {
-	return <></>;
+import Link from 'next/Link';
+import { getSortedPostsData } from '../../lib/posts.js';
+import styles from '../../styles/Blog.module.css';
+
+export async function getStaticProps() {
+	const allPostsData = getSortedPostsData();
+	return {
+		props: {
+			allPostsData,
+		},
+	};
+}
+
+export default function Blog({ allPostsData }) {
+	return (
+		<div className={styles.blog_container}>
+			<a href={'https://github.com/jokereven'} target='_new'>
+				<h2>@jokereven</h2>
+			</a>
+			<ul className={styles.blog_articles_container}>
+				{allPostsData.map(({ id, date, title, tag }) => (
+					<Link href={`/posts/${id}`} key={id}>
+						<li key={id} className={styles.blog_container_list}>
+							<img src='/source/images/blog/hacker.jpg'></img>
+							<h3>{title}</h3>
+							<br />
+							<span className={styles.time}>{date}</span>
+							<div className={styles.tag}>{tag}</div>
+						</li>
+					</Link>
+				))}
+			</ul>
+		</div>
+	);
 }
