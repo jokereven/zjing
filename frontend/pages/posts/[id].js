1d2f70f4e24490b14ed25cd6428106332ceb0a06
jokereven <zhou125disorder@163.com>
2022-03-06 18:46:26 +0800

    test(gitalk): test Gitclk on blog

--- frontend/pages/posts/[id].js
+++ frontend/pages/posts/[id].js
@@ -1,9 +1,20 @@
+import GitalkComponent from 'gitalk/dist/gitalk-component';
+import 'gitalk/dist/gitalk.css';
 import Head from 'next/head';
 import Script from 'next/script';
 import Date from '../../component/date';
 import Layout from '../../component/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import utilStyles from '../../styles/utils.module.css';
+
+var gitalk = new Gitalk({
+	clientID: 'ed231404cd5f43063891', // GitHub Application Client ID
+	clientSecret: '5e503126016b1830d93a9921d642bcb5f4691e63', // GitHub Application Client Secret
+	repo: 'zjing', // 存放评论的仓库
+	owner: 'jokereven', // 仓库的创建者，
+	admin: ['jokereven'], // 如果仓库有多个人可以操作，那么在这里以数组形式写出
+	id: ufhakdhf78y78r32, // 用于标记评论是哪个页面的，确保唯一，并且长度小于50
+});
 
 export default function Post({ postData }) {
 	return (
@@ -24,6 +35,17 @@ export default function Post({ postData }) {
 					dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
 				/>
 			</article>
+			<GitalkComponent
+				options={{
+					clientID: 'ed231404cd5f43063891',
+					clientSecret: '5e503126016b1830d93a9921d642bcb5f4691e63',
+					repo: 'zjing',
+					owner: 'cnzjing125',
+					admin: ['jokereven'],
+					id: "I don't mind being alone. I just don't want to be insignificant.", // Ensure uniqueness and length less than 50
+					distractionFreeMode: false, // Facebook-like distraction free mode
+				}}
+			/>
 		</Layout>
 	);
 }
