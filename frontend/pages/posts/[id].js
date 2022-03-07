2ce694d8f1940e92e7b7cebd7d941a0171eafd7e
jokereven <zhou125disorder@163.com>
2022-03-07 08:57:34 +0800

    fix(pages): update a color :bug:

--- frontend/pages/posts/[id].js
+++ frontend/pages/posts/[id].js
@@ -13,7 +13,7 @@ function randomString(e) {
 	var t = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678',
 		a = t.length,
 		n = '';
-	for (i = 0; i < e; i++) n += t.charAt(Math.floor(Math.random() * a));
+	for (let i = 0; i < e; i++) n += t.charAt(Math.floor(Math.random() * a));
 	return n;
 }
 
